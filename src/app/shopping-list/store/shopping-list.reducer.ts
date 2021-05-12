3c72c668c5d051c8b06df869c08935465f330988
carlos <hernandez1989carlos@gmail.com>
2021-05-12 12:22:55 -0500

    store folder was created in shopping list component to add ngrx

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,23 @@
+// import * as shoppingList from './shopping-list.action';
+// import { Ingredient } from 'src/app/share/ingredient.model';
+
+// const initialState = {
+//   ingredients: [new Ingredient('apple', 5)],
+// };
+
+// export function shoppingListReducer(
+//   state = initialState,
+//   action: shoppingList.AddIngredients
+// ) {
+//   switch (action.type) {
+//     case shoppingList.ADD_INGREDIENT:
+//       return {
+//         ...state,
+//         ingredient: state.ingredients,
+//         payload: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
